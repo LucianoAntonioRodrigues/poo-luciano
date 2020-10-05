a1568abb2e3fb306821258562f746d7f633af82a
LucianoAntonioRodrigues <larimobiliariapg@gmail.com>
2020-10-05 13:40:45 -0300

    prova
    alteração dos títulos do menu

--- prova/src/java/br/edu/fatecpg/poo/p1/Disciplina.java
+++ prova/src/java/br/edu/fatecpg/poo/p1/Disciplina.java
@@ -62,7 +62,11 @@ public class Disciplina {
         exibe.add(new Disciplina("Engenharia de Software III", "Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura. Padrões de Distribuição. Camadasno desenvolvimento de software. Tipos de Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise eProjetos. Formas de representação. O processo de desenvolvimento. Mapeamentopara implementação. Integração do sistema.Testes: planejamento e tipos.Manutenção. Documentação.","4°"));
         exibe.add(new Disciplina("Linguagem de Programação IV", "Comandos de linguagens usadas na construção e estruturação de sites para a Web, com páginas dinâmicas e interativas.Definição de layouts e formatação em geral. Adição de algoritmos usando laços, matrizes, datas, funções e condições.Introdução a Orientação a Objetos utilizando objetos, métodos e propriedades. Integração comBanco de Dados. Exercíciospráticos com projeto de criação de sites.","4°"));
         exibe.add(new Disciplina("Metodologia da Pesquisa Científico-Tecnológica", "O Papel da ciência e da tecnologia. Tipos de conhecimento. Método e técnica. Oprocesso de leitura e de análise textual.Citações e bibliografias. Trabalhos acadêmicos: tipos, características e composição estrutural. O projeto de pesquisaexperimental e não-experimental. Pesquisa qualitativa e quantitativa. Apresentação gráfica. Normasda ABNT.","4°"));
-        exibe.add(new Disciplina("Sistemas Operacionais II", "","4°"));
+        exibe.add(new Disciplina("Sistemas Operacionais II", "Apresentação de um sistema operacional específico utilizado em ambiente \n" +
+"corporativo. Requisitos de hardware para instalação do sistema. Processo de \n" +
+"instalação, personalização, operação, administração e segurança sobre o sistema \n" +
+"operacional focado. Elaboração de projetos de seleção e implantação de um sistema \n" +
+"operacional.","4°"));
         exibe.add(new Disciplina("Programação Orientada à Objetos", "Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças entre o paradigma da programaçãoestruturada em relação à orientação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo,agregação, associação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de representação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguagens deprogramação. Aplicação e uso das estruturas fundamentais da orientação a objetos.","4°"));
         return exibe;
         
