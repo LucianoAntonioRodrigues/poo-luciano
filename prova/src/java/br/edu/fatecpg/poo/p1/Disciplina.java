2a8d5d9db4c6d87c1a9ce4e70512fa68191dd09b
LucianoAntonioRodrigues <larimobiliariapg@gmail.com>
2020-11-28 19:49:05 -0300

    Prova

--- prova/src/java/br/edu/fatecpg/poo/p1/Disciplina.java
+++ prova/src/java/br/edu/fatecpg/poo/p1/Disciplina.java
@@ -12,16 +12,18 @@ import java.util.ArrayList;
  * @author Luciano
  */
 public class Disciplina {
+    
     private String nome;
     private String ementa;
     private String ciclo;
-    private Double nota;
+    private double nota;
 
     public Disciplina(String nome, String ementa, String ciclo) {
         this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
     }
+    
     
 
     public String getNome() {
@@ -48,27 +50,55 @@ public class Disciplina {
         this.ciclo = ciclo;
     }
 
-    public Double getNota() {
+    public double getNota() {
         return nota;
     }
 
-    public void setNota(Double nota) {
+    public void setNota(double nota) {
         this.nota = nota;
     }
- 
+    
     public static ArrayList<Disciplina> getList(){
-        ArrayList<Disciplina> exibe=new ArrayList<>();
-        exibe.add(new Disciplina("Banco de Dados", "Conceitos de Base de Dados. Modelos conceituais de informações. Modelos de Dados:Relacional, Redes e Hierárquicos.Modelagem de dados - conceitual, lógica e física. Teoria relacional: dependências funcionais e multivaloradas, formasnormais. Restrições deintegridade e de segurança em Banco de Dados Relacional. Sistemas Gerenciadores de Banco de Dados– objetivo e funções. Linguagens de declaração e de manipulação de dados.","4°"));
-        exibe.add(new Disciplina("Engenharia de Software III", "Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura. Padrões de Distribuição. Camadasno desenvolvimento de software. Tipos de Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise eProjetos. Formas de representação. O processo de desenvolvimento. Mapeamentopara implementação. Integração do sistema.Testes: planejamento e tipos.Manutenção. Documentação.","4°"));
-        exibe.add(new Disciplina("Linguagem de Programação IV", "Comandos de linguagens usadas na construção e estruturação de sites para a Web, com páginas dinâmicas e interativas.Definição de layouts e formatação em geral. Adição de algoritmos usando laços, matrizes, datas, funções e condições.Introdução a Orientação a Objetos utilizando objetos, métodos e propriedades. Integração comBanco de Dados. Exercíciospráticos com projeto de criação de sites.","4°"));
-        exibe.add(new Disciplina("Metodologia da Pesquisa Científico-Tecnológica", "O Papel da ciência e da tecnologia. Tipos de conhecimento. Método e técnica. Oprocesso de leitura e de análise textual.Citações e bibliografias. Trabalhos acadêmicos: tipos, características e composição estrutural. O projeto de pesquisaexperimental e não-experimental. Pesquisa qualitativa e quantitativa. Apresentação gráfica. Normasda ABNT.","4°"));
-        exibe.add(new Disciplina("Sistemas Operacionais II", "Apresentação de um sistema operacional específico utilizado em ambiente \n" +
-"corporativo. Requisitos de hardware para instalação do sistema. Processo de \n" +
-"instalação, personalização, operação, administração e segurança sobre o sistema \n" +
-"operacional focado. Elaboração de projetos de seleção e implantação de um sistema \n" +
-"operacional.","4°"));
-        exibe.add(new Disciplina("Programação Orientada à Objetos", "Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças entre o paradigma da programaçãoestruturada em relação à orientação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo,agregação, associação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de representação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguagens deprogramação. Aplicação e uso das estruturas fundamentais da orientação a objetos.","4°"));
-        return exibe;
         
+        ArrayList<Disciplina> exibe = new ArrayList<>();
+        
+        exibe.add(new Disciplina("Banco de Dados", "Conceitos de Base de Dados. Modelos conceituais de informações. Modelos de Dados:\n" +
+"Relacional, Redes e Hierárquicos. Modelagem de dados - conceitual, lógica e física. \n" +
+"Teoria relacional: dependências funcionais e multivaloradas, formas normais. \n" +
+"Restrições de integridade e de segurança em Banco de Dados Relacional. Sistemas\n" +
+"Gerenciadores de Banco de Dados – objetivo e funções. Linguagens de declaração e \n" +
+"de manipulação de dados.", "4º"));
+        exibe.add(new Disciplina("Engenharia de Software III", "Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura.\n" +
+"Padrões de Distribuição. Camadasno desenvolvimento de software. Tipos de\n" +
+"Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise e\n" +
+"Projetos. Formas de representação. O processo de desenvolvimento. Mapeamento\n" +
+"para implementação. Integração do sistema.Testes: planejamento e tipos.\n" +
+"Manutenção. Documentação.", "4º"));
+        exibe.add(new Disciplina("Linguagem de Programação IV", "Comandos de linguagens usadas na construção e estruturação de sites para a Web, \n" +
+"com páginas dinâmicas e interativas.Definição de layouts e formatação em geral. \n" +
+"Adição de algoritmos usando laços, matrizes, datas, funções e condições.Introdução a \n" +
+"Orientação a Objetos utilizando objetos, métodos e propriedades. Integração com\n" +
+"Banco de Dados. Exercíciospráticos com projeto de criação de sites.", "4º"));
+        exibe.add(new Disciplina("Metodologia da Pesquisa Científico-Tecnológica", "O Papel da ciência e da tecnologia. Tipos de conhecimento. Método e técnica. O\n" +
+"processo de leitura e de análise textual.Citações e bibliografias. Trabalhos acadêmicos: \n" +
+"tipos, características e composição estrutural. O projeto de pesquisaexperimental e \n" +
+"não-experimental. Pesquisa qualitativa e quantitativa. Apresentação gráfica. Normas\n" +
+"da ABNT.", "4º"));
+        exibe.add(new Disciplina("Programação Orientada à Objetos", "Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças \n" +
+"entre o paradigma da programaçãoestruturada em relação à orientação a objetos. \n" +
+"Conceito de objeto, classe, métodos, atributos, herança, polimorfismo,agregação, \n" +
+"associação, dependência, encapsulamento, mensagem e suas respectivas notações na \n" +
+"linguagem padrão de representação da orientação a objetos. Implementação de \n" +
+"algoritmos orientado a objetos utilizando linguagens deprogramação. Aplicação e uso \n" +
+"das estruturas fundamentais da orientação a objetos.", "4º"));
+        exibe.add(new Disciplina("Sistemas Operacionais II", "Apresentação de um sistema operacional específico utilizado em ambiente \n" +
+"corporativo. Requisitos de hardware parainstalação do sistema. Processo de\n" +
+"instalação, personalização, operação, administração e segurança sobre o sistema\n" +
+"operacional focado. Elaboração de projetos de seleção e implantação de um sistema\n" +
+"operacional.", "4º"));
+        
+        return exibe;
     }
+    
+    
 }
